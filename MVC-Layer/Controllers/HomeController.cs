93a7121730b55f8fc422c9d3445c65319fec1f7e
Erkan Gorgulu <erkangorgulu@gmail.com>
2019-12-17 14:28:44 +0300

    homepage ui created

---
+++ MVC-Layer/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Layer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Tişört ve Tasarım Ürünler | KAFT";
+            return View();
+        }
+    }
+}
